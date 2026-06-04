c38f9e7b1bd2c4764177e4476f6732dc18292803
aryankhalati <aryankhalati9@gmail.com>
2026-06-04 10:35:00 +0530

    feat: connect MongoDB and update Todo controller

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -1,25 +1,35 @@
-let todos = []; // temp memory
-let  nextId = 1; // temp memory
 
-const getAllTodos = (req, res) => {
-    res.status(200).json(todos);
-}; //run when GET /api/todos, 200=> success
 
-const createTodo = (req,res) => {
-    const { title } = req.body;  //data sent by client
-    const newTodo = {
-        id:nextId++,
-        title: title,
-        createdAt: new Date() // build new todo object, give ID, title and timestamp
-    }; 
-    todos.push(newTodo); //push into array and send back
-    res.status(201).json(newTodo); //success 201
-};
+const Todo = require('../models/Todo');
 
-const deleteTodo = (req, res) => {
-    const { id } = req.params; //ID from url, if DELETE, gets ID in string form
-    todos = todos.filter(todo => todo.id !== parseInt(id)); //Filter matching ID, convert string to int
-    res.status(200).json({ message: 'Todo deleted sucessfully'}); //send success message
-};
+ const getAllTodos = async (req, res) => { //async coz data fetching takes time
+    try{
 
+        const todos = await Todo.find(); // .find fetch all todo docs, await wait until done
+        res.status(200).json(todos); //success message via 200
+    }
+    catch (error){ //if any prblm, catch it and show error
+        res.status(500).json({message : error.message});  //500 - server error
+
+    }
+ };
+const createTodo = async (req, res) => {
+    try{
+        const tocreate = await Todo.create(req.body);
+        res.status(201).json(tocreate);
+    }
+    catch (error) {
+        res.status(500).json({message : error.message});
+    }
+}
+const deleteTodo = async (req, res) => {
+    try {
+        const todelete = await Todo.findByIdAndDelete(req.params.id)
+        res.status(200).json(todelete);
+    }
+    catch (error) {
+           res.status(500).json({message : error.message});
+    }
+}
 module.exports = {getAllTodos, createTodo, deleteTodo}; // export to todoRoutes.js and use them
+
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -11,6 +11,6 @@ const todoSchema = new mongoose.Schema({
     }
 }); // schema = blueprint of data, required: true means title cant be emnpty, default: Date.now - use default time if not mentioned
 
-const Todo = mongoose.model('Todo, todoSchema'); // create actual Model from schema, 
+const Todo = mongoose.model('Todo', todoSchema); // create actual Model from schema, 
 
 module.exports = Todo // controller can import and use it to save,find, delete data
