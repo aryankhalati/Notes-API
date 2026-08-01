7ba69ee26ce64338bdef7440067551142b7ea450
aryankhalati <aryankhalati9@gmail.com>
2026-08-01 21:06:59 +0530

    Surface Resend API errors instead of silently swallowing them

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,12 +3,19 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-    await resend.emails.send({
+    const { data, error } = await resend.emails.send({
         from: 'Notes API <onboarding@resend.dev>',
         to,
         subject,
         html
     });
+
+    if (error) {
+        console.error('Resend error:', error);
+        throw new Error(error.message || 'Failed to send email');
+    }
+
+    console.log('Email sent:', data);
 };
 
 module.exports = sendEmail;
