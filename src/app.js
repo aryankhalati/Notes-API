4ad75d5236055b07cab68d3f712052ff27684f3c
aryankhalati <aryankhalati9@gmail.com>
2026-08-01 19:38:18 +0530

    Fix SMTP IPv6 issue and trust proxy config for production

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const noteRoutes = require('./routes/noteRoutes');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(express.json());
 app.use(helmet());
